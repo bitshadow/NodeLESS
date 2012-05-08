6fc6d42e4866325983dcd9e74ac73395f001a95c
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2012-05-08 14:06:25 +0530

    Added NodeLESS and README

---
+++ Node-Less.js
@@ -0,0 +1,39 @@
+var less = require('less'),
+    path = require('path'),
+    join = path.join,
+    fs = require('fs');
+
+
+generate = function(fname) {
+  var path, lessparser, contents;
+
+  path = join(__dirname, fname);
+  if(!fname.match(/\.less$/) || !fs.statSync(path).isFile())
+    return;
+  lessparser = new less.Parser({
+    paths: [__dirname],
+    filename: fname
+  });
+
+  contents = fs.readFileSync(path).toString();
+  console.log(fname + "\n~~~~~~~~~~\n" + contents+"\n" )
+  lessparser.parse(contents, function(err, tree){
+    if(err){
+      console.log(err);
+      throw new Error(err);
+    }
+
+    var cssfname = fname.replace(/less$/, 'css');
+    fs.writeFileSync(join(__dirname,cssfname), tree.toCSS());
+    console.log(cssfname +"\n~~~~~~~~~\n" + tree.toCSS())
+  });
+}
+main = function () {
+  var arglen = process.argv.length;
+  if(arglen == 3 )
+    generate(process.argv[2]);
+  else
+    console.log("required number of arguments 3 provided " + argslen );
+}
+
+main();
--- README.md
+++ README.md
@@ -2,3 +2,22 @@ NodeLESS
 ========
 
 Use LESS with Node to generate .css files from .less files.( as middleware )
+
+Usage
+~~~~~
+
+1) Download and compile: https://github.com/cloudhead/less.js/
+
+2) git clone git@github.com:jigneshhk/NodeLESS.git
+
+3) cd NodeLESS
+
+4) Create filename.less contains less content.
+
+5) node Node-Less.js filename.less
+
+6) See generated content in filename.css
+
+PS: filename.less (example: 'foo.less') should be inside NodeLESS folder
+    example : ~/NodeLess/abc.less
+
